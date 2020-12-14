02457698a4293dc1b9d6f209c7bf542a1ceb05d2
mondevyat <54996368+mondevyat@users.noreply.github.com>
2020-12-14 14:32:33 +0300

    Update Model.js

--- Lab4/Model.js
+++ Lab4/Model.js
@@ -1,17 +1,17 @@
-const GAME_FIELD_SIZE = 10;
+const GAME_FIELD_SIZE = 5;
 const GAME_SELL_SIZE = 100;
 
 var Model = function() {
 	this.objs = {
 		bombs: [
 			{
-			   x: parseInt(Math.random() * (GAME_FIELD_SIZE*GAME_SELL_SIZE - 1) + 1),
-			   y: parseInt(Math.random() * (GAME_FIELD_SIZE*GAME_SELL_SIZE - 1) + 1),
+			   x: parseInt(Math.random() * (GAME_FIELD_SIZE) + 1),
+			   y: parseInt(Math.random() * (GAME_FIELD_SIZE) + 1),
 			   isExploded: false
 			},
 			{
-			   x: parseInt(Math.random() * (GAME_FIELD_SIZE*GAME_SELL_SIZE - 1) + 1),
-			   y: parseInt(Math.random() * (GAME_FIELD_SIZE*GAME_SELL_SIZE - 1) + 1),
+			   x: parseInt(Math.random() * (GAME_FIELD_SIZE) + 1),
+			   y: parseInt(Math.random() * (GAME_FIELD_SIZE) + 1),
 			   isExploded: false
 			},
 		],
@@ -20,16 +20,27 @@ var Model = function() {
 		mas: [],
 		flagCounter: 0,
 		isMapCreated: false,
-		isGameEnded: false,
-		trigger: -1
+		isGameEnded: false
 	};
 }
 
 Model.prototype.init = function(renderFunc) {
 	this.needRendering = renderFunc;
-	
+    
 	for( var i = 0; i < GAME_FIELD_SIZE; i++ )
-         this.objs.mas[i] = [{posX: 0, posY: 0, offsetX: 0, offsetY: 0, isBomb: false, isChecked: false, isFlagged: false}];
+	{
+		this.objs.mas[i] = [];
+		for (var j = 0; j < GAME_FIELD_SIZE; j++) {
+			this.objs.mas[i][j] = {posX: 0, posY: 0, offsetX: 0, offsetY: 0, isBomb: false, isChecked: false, isFlagged: false};
+			for (var k = 0; k < this.objs.bombs.length; k++)
+    		{
+    		    if ( (this.objs.bombs[k].x === i && this.objs.bombs[k].y === j))
+    		    { 
+    		        this.objs.mas[i][j].isBomb = true;
+    		    }
+    		}
+		}
+	}
 };
 
 Model.prototype.getBombs = function (obj) {
@@ -54,61 +65,44 @@ Model.prototype.getCoordinates = function (x, y)  {
 	}
 }
 
-Model.prototype.clickCell = function (e, x, y) { 
+Model.prototype.clickCell = function (e, i, j) { 
     switch (e) {
       case 1: {
 		console.log("левая");
-		for (var i = 0; i < GAME_FIELD_SIZE; i++) 
+		if (!this.objs.mas[i][j].isChecked && !this.objs.mas[i][j].isBomb)
 		{
-			for (var j = 0; j < GAME_FIELD_SIZE; j++)
-			{
-				if ( (x >= this.objs.mas[i][j].posX && x <= this.objs.mas[i][j].posX + this.objs.mas[i][j].offsetX) &&
-					 (y >= this.objs.mas[i][j].posY && y <= this.objs.mas[i][j].posY + this.objs.mas[i][j].offsetY) &&
-					 !this.objs.mas[i][j].isChecked && !this.objs.mas[i][j].isBomb)
-				{
-					this.objs.mas[i][j].isChecked = true;
-					this.objs.score++;
-					console.log("КЛЕТКА №" + parseInt(i) + '' + parseInt(j));
-				}
-				else if ( (x >= this.objs.mas[i][j].posX && x <= this.objs.mas[i][j].posX + this.objs.mas[i][j].offsetX) &&
-					 	(y >= this.objs.mas[i][j].posY && y <= this.objs.mas[i][j].posY + this.objs.mas[i][j].offsetY) &&
-					 	this.objs.mas[i][j].isBomb && !this.objs.mas[i][j].isChecked)
-				{
-					this.objs.isGameEnded = true;
-					console.log("КЛЕТКА №" + parseInt(i) + '' + parseInt(j));
-					console.log(this.objs.isGameEnded);
-				}
-			}
+			this.objs.mas[i][j].isChecked = true;
+			this.objs.score++;
+			console.log("КЛЕТКА №" + parseInt(i) + '' + parseInt(j));
+		}
+		else if (this.objs.mas[i][j].isBomb && !this.objs.mas[i][j].isChecked)
+		{
+			this.objs.isGameEnded = true;
+			console.log("КЛЕТКА №" + parseInt(i) + '' + parseInt(j));
+			console.log(this.objs.isGameEnded);
 		}
 		this.needRendering();
         break;
       }
       case 3: {
 		console.log("правая");
-		for (var i = 0; i < GAME_FIELD_SIZE; i++) 
+		
+		if ( (!this.objs.mas[i][j].isChecked) )
 		{
-			for (var j = 0; j < GAME_FIELD_SIZE; j++)
+			if (this.objs.mas[i][j].isFlagged == false && this.objs.flagCounter < 5)
 			{
-				if ( (x >= this.objs.mas[i][j].posX && x <= this.objs.mas[i][j].posX + this.objs.mas[i][j].offsetX) &&
-					 (y >= this.objs.mas[i][j].posY && y <= this.objs.mas[i][j].posY + this.objs.mas[i][j].offsetY) 
-					  && (!this.objs.mas[i][j].isChecked) )
-				{
-					if (this.objs.mas[i][j].isFlagged == false && this.objs.flagCounter < 5)
-					{
-						this.objs.flagCounter++;
-						this.objs.mas[i][j].isFlagged = true;
-						console.log("флажок поставлен " + this.objs.flagCounter);
-						this.needRendering();
-					}
-					else if (this.objs.mas[i][j].isFlagged == true && this.objs.flagCounter > 0) {
-						this.objs.flagCounter--;
-						this.objs.mas[i][j].isFlagged = false;
-						console.log("флажок убран " + this.objs.flagCounter);
-						this.needRendering();
-					}
-					console.log("КЛЕТКА №" + parseInt(i) + '' + parseInt(j));
-				}
+				this.objs.flagCounter++;
+				this.objs.mas[i][j].isFlagged = true;
+				console.log("флажок поставлен " + this.objs.flagCounter);
+				this.needRendering();
 			}
+			else if (this.objs.mas[i][j].isFlagged == true && this.objs.flagCounter > 0) {
+				this.objs.flagCounter--;
+				this.objs.mas[i][j].isFlagged = false;
+				console.log("флажок убран " + this.objs.flagCounter);
+				this.needRendering();
+			}
+			console.log("КЛЕТКА №" + parseInt(i) + '' + parseInt(j));
 		}
 		this.needRendering();
         break;
